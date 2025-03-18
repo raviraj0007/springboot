01d5ecd630a412cdc488f57ac47fb978ed020a48
raviraj0007 <raviraj04066@gmail.com>
2025-03-19 00:34:54 +0530

    Initial commit

---
+++ app/src/main/java/org/example/service/OrderService.java
@@ -0,0 +1,9 @@
+package org.example.service;
+
+import org.example.bean.UserConfig;
+
+public class OrderService {
+    public static OrderService createInstance(UserService userservice, UserConfig userconfig){
+        return null;
+    }
+}
---
+++ app/src/main/java/org/example/service/UserService.java
@@ -0,0 +1,12 @@
+package org.example.service;
+
+import org.example.bean.UserConfig;
+
+public class UserService {
+
+    UserConfig userConfig;
+
+    public UserService(UserConfig userConfig) {
+        this.userConfig = userConfig;
+    }
+}
